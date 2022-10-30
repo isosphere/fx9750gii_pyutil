2bf03d5ee545dd3d6f0b279877f6f715ceca85db
Matthew Scheffel <matt@dataheck.com>
2022-10-30 14:42:40 -0300

    Update util.py

--- util.py
+++ util.py
@@ -3,8 +3,9 @@ import casioplot as cp
 # These are empirically determined from my fx-9750GIII v. 360_2b
 SCREEN_WIDTH = 130 # pixels
 SCREEN_HEIGHT = 64 # pixels
-CHAR_HEIGHT = 5    # pixels
-CHAR_WIDTH = 4     # pixels
+# MEDIUM is the default font size
+MEDIUM_CHAR_HEIGHT = 5    # pixels
+MEDIUM_CHAR_WIDTH = 4     # pixels
 MIN_FRAME_TIME = 0.25 # let a frame last this long with sleep
 
 def sleep(sec):
